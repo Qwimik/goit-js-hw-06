cc0957791ab3cd649a90e9be04b04d534e9f752d
Oleksii Rozhnenko <qwimik@gmail.com>
2023-02-04 22:23:43 +0200

    complete task-01

---
+++ src/js/task-01.js
@@ -0,0 +1,12 @@
+const categoriesEl = document.querySelector('#categories');
+
+console.log(`Number of categories: ${categoriesEl.children.length}`);
+
+const itemsEl = document.querySelectorAll('.item');
+
+console.log(
+  itemsEl.forEach(item => {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`);
+  })
+);
