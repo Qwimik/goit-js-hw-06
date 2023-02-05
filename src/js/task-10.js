50f850653de0466affa4e315002bd1bb31358b07
Oleksii Rozhnenko <qwimik@gmail.com>
2023-02-05 15:15:01 +0200

    complete task-10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,27 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBtnEl = document.querySelector('[data-create]');
+const destroyBtnEl = document.querySelector('[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+createBtnEl.addEventListener('click', createBoxes);
+destroyBtnEl.addEventListener('click', destroyBoxes);
+
+let divWidth = 20;
+
+function createBoxes(amount) {
+  let divEl = '';
+
+  for (let i = 1; i <= amount.target.previousElementSibling.value; i++) {
+    divWidth += 10;
+    divEl += `<div style="background-color: ${getRandomHexColor()}; width: ${divWidth}px; height: ${divWidth}px;"></div>`;
+  }
+  boxesEl.insertAdjacentHTML('beforeend', divEl);
+}
+
+function destroyBoxes() {
+  divWidth = 20;
+  boxesEl.innerHTML = '';
+}
