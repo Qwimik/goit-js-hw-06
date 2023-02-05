d012c9dfaa138c4167a1093d3455675c7d053f3f
Oleksii Rozhnenko <qwimik@gmail.com>
2023-02-05 14:14:50 +0200

    complete task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form');
 
+const result = {};
+
+formEl.addEventListener('submit', onFormElSubmit);
+
+function onFormElSubmit(e) {
+  e.preventDefault();
+
+  if (
+    formEl.elements.email.value === '' ||
+    formEl.elements.password.value === ''
+  ) {
+    return alert('Всі поля повинні бути заповнені!');
+  }
+
+  result.email = formEl.elements.email.value;
+  result.password = formEl.elements.password.value;
+
+  console.log(result);
+
+  formEl.reset();
+}
