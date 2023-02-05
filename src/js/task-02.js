04c3a69565503d4cbfaa8db9bc884dac55cdbaa3
Oleksii Rozhnenko <qwimik@gmail.com>
2023-02-05 12:39:11 +0200

    complete task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const itemsOfIngredients = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = ingredient;
+  return itemEl;
+});
+
+ingredientsEl.append(...itemsOfIngredients);
