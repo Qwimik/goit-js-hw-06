578c8d63d4476642b4a9e04aa8869f6505d626e5
Oleksii Rozhnenko <qwimik@gmail.com>
2023-02-05 14:26:48 +0200

    complete task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnEl = document.querySelector('.change-color');
+const textEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+
+btnEl.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  textEl.textContent = color;
+});
